4a34d116b02dc5ac96a69564d7bbe219248353ae
RubeRad <ruberad@gmail.com>
2020-08-25 07:32:37 -0700

    Original Knuth version of MMIX 500 primes, from TAoCP fascicle 1 Very hard to understand because 'This program has deliberatly been written in a slightly clumsy fashion in order to illustrate most of the features of MIXAL in a single program'.

--- mnist.py
+++ mnist.py
@@ -2,6 +2,7 @@
 
 import random
 import os
+import sys
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
@@ -46,17 +47,17 @@ print('tensorflow version is', tf.__version__)
 # the module gives easy access to the test data
 # first run this will download to a local file
 # subsequent runs it will be much quicker
-fashion_mnist = keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+#fashion_mnist = keras.datasets.fashion_mnist
+#(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+#class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+#               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-#number_mnist = keras.datasets.mnist
-#(train_images, train_labels), (test_images, test_labels) =  number_mnist.load_data()
-#class_names = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+number_mnist = keras.datasets.mnist
+(train_images, train_labels), (test_images, test_labels) =  number_mnist.load_data()
+class_names = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 # see what the data looks like
-show_data = False
+show_data = (len(sys.argv) > 1)
 if show_data:
    plt.figure(figsize=(10,10))
    for i in range(25):
@@ -93,14 +94,30 @@ else:
                  metrics=['accuracy'])
 
    # this is the training step that takes all the time
-   model.fit(train_images, train_labels, epochs=10)
+   model.fit(train_images, train_labels, epochs=5)
 
    with open('model.json', 'w') as file:
       file.write(model.to_json())
    model.save_weights('model.h5')
    print('saved model to files model.json and model.h5')
 
-# either way now we have a model with weights
+
+# either way now we have a model with weights; write them out to csv
+i = 0
+for a in model.get_weights():
+   fname = 'model.weights'+str(i)+'.csv'
+   i += 1
+   with open(fname, 'w') as csv:
+      if len(a.shape)==2:
+         rows, cols = a.shape
+         for r in range(rows):
+            for c in range(cols):
+               csv.write(str(a[r][c]) + ',')
+            csv.write("\n")
+      else:
+         rows = a.shape[0]
+         for r in range(rows):
+            csv.write(str(a[r]) + "\n")
 
 
 test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
@@ -112,12 +129,12 @@ print('Test accuracy:', test_acc)
 predictions = model.predict(test_images)
 
 
-num_rows = 5
-num_cols = 3
+num_rows = 10
+num_cols = 6
 num_images = num_rows*num_cols
 plt.figure(figsize=(2*2*num_cols, 2*num_rows))
 for ii in range(num_images):
-  i = random.randint(0,len(test_images))
+  i = random.randint(0,len(test_images)-1)
   plt.subplot(num_rows, 2*num_cols, 2*ii+1)
   plot_image(i, predictions[i], test_labels, test_images)
   plt.subplot(num_rows, 2*num_cols, 2*ii+2)
